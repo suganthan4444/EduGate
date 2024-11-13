f27e9408fe4d2004e3c85aa15b6c4e87239fd9bd
sugu <114516437+suganthan4444@users.noreply.github.com>
2024-11-13 11:02:58 +0530

    Update settings.py

--- edugatedjangoproject/settings.py
+++ edugatedjangoproject/settings.py
@@ -158,7 +158,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
 EMAIL_HOST_USER = 'edugate.ed@gmail.com' 
-EMAIL_HOST_PASSWORD = 'qhul lsoa eqeb ysyj' 
+EMAIL_HOST_PASSWORD = '' 
 
 
 import os
