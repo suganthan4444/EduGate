bba1b16924a184dde8528908c219f24a499e24e5
aksuganthan@gmail.com <aksuganthan@gmail.com>
2025-03-15 17:11:38 +0530

    Changed env variables

--- edugatedjangoproject/settings.py
+++ edugatedjangoproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
