9dd45d7bc62816317c08de21bf46ff9ce45ce38b
sugu <114516437+suganthan4444@users.noreply.github.com>
2025-03-26 15:43:18 +0530

    Update settings.py

--- edugatedjangoproject/settings.py
+++ edugatedjangoproject/settings.py
@@ -40,14 +40,14 @@ SECRET_KEY = env('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-X_FRAME_OPTIONS = 'ALLOW-FROM http://localhost:3000 http://127.0.0.1:8000 https://suganthan4444.github.io/EduGate/ https://edu-gate.vercel.app/'
+X_FRAME_OPTIONS = 'ALLOW-FROM http://localhost:3000 http://127.0.0.1:8000 https://suganthan4444.github.io/EduGate/ https://edu-gate.vercel.app/ https://edu-gate-suganthans-projects-822936bb.vercel.app/'
 
 
 
-ALLOWED_HOSTS = ['localhost', 'localhost:3000','127.0.0.1', 'https://suganthan4444.github.io/EduGate/', 'https://edu-gate.vercel.app/']
+ALLOWED_HOSTS = ['localhost', 'localhost:3000','127.0.0.1', 'https://suganthan4444.github.io/EduGate/', 'https://edu-gate.vercel.app/', 'https://edu-gate-suganthans-projects-822936bb.vercel.app/']
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [ "http://localhost:3000", 'http://127.0.0.1:3000', "https://suganthan4444.github.io/EduGate/ ", "https://edu-gate.vercel.app/"] 
+CORS_ALLOWED_ORIGINS = [ "http://localhost:3000", 'http://127.0.0.1:3000', "https://suganthan4444.github.io/EduGate/ ", "https://edu-gate.vercel.app/", "https://edu-gate-suganthans-projects-822936bb.vercel.app/"] 
 CORS_ALLOW_METHODS = [
     'GET',
     'POST',
@@ -64,7 +64,7 @@ CORS_ALLOW_HEADERS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_USE_SESSIONS= True
-CSRF_TRUSTED_ORIGINS=["http://localhost:3000", "https://suganthan4444.github.io/EduGate/", "https://edu-gate.vercel.app/"]
+CSRF_TRUSTED_ORIGINS=["http://localhost:3000", "https://suganthan4444.github.io/EduGate/", "https://edu-gate.vercel.app/", "https://edu-gate-suganthans-projects-822936bb.vercel.app/"]
 
 # Application definition
 
