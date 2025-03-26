31c9bb52e6922cbe75146d643d1bfc2c3f2242d4
sugu <114516437+suganthan4444@users.noreply.github.com>
2025-03-26 15:14:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -22,7 +22,7 @@ export const fetchEducatorSessionData = async () => {
 };
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
+    baseURL: process.env.REACT_APP_API_URL || 'https://edu-gate.vercel.app/api',
     timeout: 2*60*1000, 
     withCredentials: true,
 });
